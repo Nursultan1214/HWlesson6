77101283f74db6cd329d3826cb5c52724e4c99eb
Nursultan1214 <Nurchik1214@mail.ru>
2021-04-26 22:48:37 +0600

    version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,9 @@ public class Main {
         Boss.setHealth(660);
         Boss.setDamage(44);
         Boss.setAttack("flash");
-        System.out.println("Информация про босса:  " + Boss.getHealth()  +  Boss.getAttack()  +  Boss.getDamage());
-    }
-}
+               System.out.println("Информация про босса:  " + Boss.getHealth ()  +  Boss.getAttack ()  +  Boss.getDamage());
+               System.out.println(Boss.getHealth ());
+               System.out.println(Boss.getAttack ());
+               System.out.println(Boss.getDamage());
+
+}}
